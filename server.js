2df08c56ab54235f568838ade7230349bdef382a
Andrew Murriel <murriela1@gmail.com>
2016-08-05 17:16:37 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
-var PORT = process.env.port || 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
